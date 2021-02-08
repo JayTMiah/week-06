e8e9c04ce32a74c4fe69621cdf6dfa94c09e4c8b
Jay Miah <jmiah@memberinsurance.com>
2021-02-08 19:08:43 +0000

    Jay - adding 09 again because of mistakes

---
+++ 02-arrays-php/09-squares.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// … your function here
+
+dump(
+    squares([2, 3, 4]), // [4, 9, 16]
+    squares([2, 3, 4, 5, 6]), // [4, 9, 16, 25, 36]
+);
