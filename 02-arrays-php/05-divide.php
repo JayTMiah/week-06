a7ca62a7e92a22a5b7ce50b89c40ec9394571edd
Jay Miah <jmiah@memberinsurance.com>
2021-02-08 16:46:25 +0000

    Jay - Answer for 05-divide

---
+++ 02-arrays-php/05-divide.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function divide($number1, $number2) {
+    return $number1 / $number2;
+}
+
+// … your function here
+
+dump(divide(4, 2)); // 2
+dump(divide(20, 2)); // 10
+dump(divide(5, 2)); // 2.5
+dump(divide(2.5, 0.5)); // 5
